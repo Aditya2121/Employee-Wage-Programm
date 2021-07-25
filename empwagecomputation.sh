8e5dc9db45ba88cc351bc6df457763fd7cd6e65d
Aditya Gour <adityagour442@gmail.com>
2021-07-25 17:10:36 +0530

    Add part time Employee wage

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -3,15 +3,21 @@ echo "Welcome to Employee Wage Computation Programm"
 
 ispresent=1
 isabsent=0
+isparttime=3
 Wageperhour=20
-Check=$((RANDOM%2))
+Check=$((RANDOM%3))
 echo $Check
 if [ $Check -eq  1 ]
 then
 	echo "Empployee is present"
 	EmployeeWage=$((8*20))
-	echo "The Daily Employee Wage is" $EmployeeWage
+	echo "The Full Time Employee Wage is" $EmployeeWage
+elif [ $Check -eq 2 ]
+then
+	echo "Employee is part time present"
+	 EmployeeWage=$((8*20))
+	 echo "The Daily Employee Wage is" $EmployeeWage
 else
-	 echo "Empployee is Absent"
+	echo "Employee is Absent"
 fi
 
