1816f6430533bf2d8b8f3835cedb6a5a88ffa22d
Aditya Gour <adityagour442@gmail.com>
2021-07-25 16:41:55 +0530

    Checking Daily Employee Wage

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -3,11 +3,15 @@ echo "Welcome to Employee Wage Computation Programm"
 
 ispresent=1
 isabsent=0
+Wageperhour=20
 Check=$((RANDOM%2))
 echo $Check
-if [ $Check - eq  1 ]
+if [ $Check -eq  1 ]
 then
 	echo "Empployee is present"
+	EmployeeWage=$((8*20))
+	echo "The Daily Employee Wage is" $EmployeeWage
 else
 	 echo "Empployee is Absent"
 fi
+
