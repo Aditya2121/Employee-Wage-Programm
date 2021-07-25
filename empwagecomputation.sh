924cf5dedfc8cbe6486ec8ff9f2ffaea600ed9ea
Aditya Gour <adityagour442@gmail.com>
2021-07-25 17:39:25 +0530

    Wages for a 20 workingdays per month

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -5,17 +5,18 @@ ispresent=1
 isabsent=0
 isparttime=3
 Wageperhour=20
+workingdaypermonth=20
 Check=$((RANDOM%3))
 echo $Check
 if [ $Check -eq  1 ]
 then
 	echo "Empployee is present"
-	EmployeeWage=$((8*20))
+	EmployeeWage=$((8*20*20))
 	echo "The Full Time Employee Wage is" $EmployeeWage
 elif [ $Check -eq 2 ]
 then
 	echo "Employee is part time present"
-	 EmployeeWage=$((8*20))
+	 EmployeeWage=$((8*20*20))
 	 echo "The Daily Employee Wage is" $EmployeeWage
 else
 	echo "Employee is Absent"
@@ -23,11 +24,11 @@ fi
 
 	case $Check  in 
 		   1) echo "Employee is Full time Present "
-                EmployeeWage=$((8*20))
+                EmployeeWage=$((8*20*20))
                 echo "Employee Full time Present wage is"$EmployeeWage
                         ;;
          2) echo "Employee is Parttime Present "
-                EmployeeWage=$((8*20))
+                EmployeeWage=$((8*20*20))
                 echo "Employee Part time Present wage is"$EmployeeWage
                                 ;;
         *) echo "Employee is absent"
